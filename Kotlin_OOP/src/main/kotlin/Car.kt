fe4cffd2a521ae7d551440afb63227f4a4bc4617
HICHAMSEBAA <hichamsebaa45@gmail.com>
2023-02-28 22:26:22 +0100

    add function in Car class (Display) for dislplay genrale info about car object

--- Kotlin_OOP/src/main/kotlin/Car.kt
+++ Kotlin_OOP/src/main/kotlin/Car.kt
@@ -1,3 +1,13 @@
-class Car () {
+import jdk.jfr.Category
 
+class Car (private val Category: String,private val Brand: String,private val Model: String,private val Year: Int) {
+
+    //Display Function
+    fun Display(){
+        println("*** General Information About this Car ***" )
+        println(" Category : ${this.Category}")
+        println(" Brand    : ${this.Brand}")
+        println(" Model    : ${this.Model}")
+        println(" Year     : ${this.Year}")
+    }
 }
